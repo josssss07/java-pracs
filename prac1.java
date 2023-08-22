b6ff6d609b32b8607528cc318c1c082bbc9d6f79
josssss07 <joshuadesai03@gmail.com>
2023-08-22 09:24:46 +0530

    rest of java pracs

---
+++ prac1.java
@@ -0,0 +1,85 @@
+//How to use array list for a student database
+
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student{
+	private int  rno;
+	private String name;
+	private String course;
+	private int age;
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	Student(int rno, String name, String course, int age){
+		this.rno = rno;
+		this.name= name; 
+		this.course = course;
+		this.age = age;
+	}
+	
+}
+
+public class prac1{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub;\
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Student> ar = new ArrayList<Student>();
+		
+		for(int i= 0; i<6; i++) {
+			
+			
+			System.out.println(" record no: " + (i+1));
+			
+			System.out.print("enter the rno:" );
+			int r = sc.nextInt();
+			System.out.println(" enter name: ");
+			String n = sc.nextLine();
+			sc.next();
+			System.out.println("Enter the course: ");
+			String c = sc.nextLine();
+			sc.next();
+			System.out.print("Enter the age: ");
+			int a= sc.nextInt();
+			
+			
+			Student st = new Student(r, n , c, a);
+			ar.add(st);
+			
+		}
+		
+		System.out.println("Enter the roll no to search");
+		int rno_search = sc.nextInt();
+		
+		for(Student s:ar) {
+			if(s.getRno() == rno_search) {
+				System.out.println(s.getName()+ " " + s.getCourse()+ " "+ s.getAge()+" " + s.getAge());
+			}
+		}
+	}
+}
---
+++ prac2&3.java
@@ -0,0 +1,150 @@
+
+//Q1 convert alternating words to uppercase:
+
+
+
+public class prac2 {
+	
+	public static void main(String[] args) {
+	String s = "We are learning about strings";
+	
+	prac2 m = new prac2();
+	
+	m.alternate(s);
+}
+
+
+void alternate(String s) {
+	String newString = "";
+	
+	String[] sarr = s.split("\\s+");
+	
+	for(int i = 0; i<sarr.length; i++) {
+		if(i%2 == 0) {
+			newString = newString + sarr[i].toUpperCase()+ " ";
+		}
+		else {
+			newString = newString + sarr[i] + " ";
+		}
+		
+	}
+	
+	System.out.println(newString);
+}
+}
+
+
+
+
+//Q2 Write a method called display which shows only the center word from a sentence
+
+//Q3 Write a method to find the longest word in a string 
+
+//Q5 Reverse alternate words in a string 
+
+//Q6 Write a method that deletes string word from the sentence 
+
+public class Student {
+	
+	public static void main(String[] args) {
+	String s = "We are learning about strings";
+	
+	Student m = new Student();
+	
+	m.alternate(s);
+	m.longestWord(s);
+	m.middleWord(s);
+	m.stringReverse(s);
+	m.alternateRevers(s);
+}
+
+
+	
+	//Q1`
+	void alternate(String s) {
+		String newString = "";
+		
+		String[] sarr = s.split("\\s+");
+		
+		for(int i = 0; i<sarr.length; i++) {
+			if(i%2 == 0) {
+				newString = newString + sarr[i].toUpperCase()+ " ";
+			}
+			else {
+				newString = newString + sarr[i] + " ";
+			}
+			
+		}
+		
+		System.out.println(newString);
+}
+	//O2
+	
+	void longestWord(String s) {
+		String longest="";
+		
+		String[] sarr = s.split("\\s+");
+		int max= 0;
+		String maxstr= "";
+		for(String str: sarr) {
+			int n = str.length();
+			if(n> max) {
+				max = n;
+				maxstr = str;
+			}
+				
+		}
+		System.out.println(maxstr);
+	}
+	
+	
+	//Q3
+	
+	void middleWord(String s) {
+		
+		String[] sarr = s.split("\\s+");
+		
+		String newString = "";
+		
+		
+		int strLenght = sarr.length;
+		int length = strLenght;
+		if(length % 2 == 0) {
+			newString = sarr[(length/2)-1] + " " + sarr[(length/2)];
+		}
+		
+		else {
+			newString = sarr[(length/2)];
+		}
+		
+		System.out.println("Middle word is "+ newString);
+	}
+	
+	
+	void stringReverse(String s) {
+		StringBuffer sb = new StringBuffer(s);
+		sb.reverse();
+		System.out.println(sb);
+	}
+	
+	void alternateRevers(String s) {
+		String newString = "";
+		
+		String[] sarr = s.split("\\s+");
+		
+		for(int i = 0; i<sarr.length; i++) {
+			if(i%2 == 0) {
+				StringBuffer sb = new StringBuffer(sarr[i]);
+				newString = newString + sb.reverse() + " ";
+				
+			}
+			else {
+				newString = newString + sarr[i] + " ";
+			}
+			
+		}
+		
+		System.out.println(newString);
+}
+}
+
